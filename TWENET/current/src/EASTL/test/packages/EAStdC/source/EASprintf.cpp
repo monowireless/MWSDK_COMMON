60bef69fa20105dacb5edf3a08d665056510c509
monowireless <monowireless@mono-wireless.com>
2022-02-17 12:40:42 +0900

    - remove unnecessary files - added sprintf supporting code

--- TWENET/current/src/EASTL/test/packages/EAStdC/source/EASprintf.cpp
+++ TWENET/current/src/EASTL/test/packages/EAStdC/source/EASprintf.cpp
@@ -4,14 +4,27 @@
 // Copyright (c) Electronic Arts. All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
+#if defined(JN516x) || defined(CPU_JN518X)
+#include <EABase/eabase.h>
+#include <EAStdC/EASprintf.h>
+// use TWENET lib's printf library (see TWENETmcu/printf for details)
+extern "C" int vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
+#else
 #include <EABase/eabase.h>
 #include <EAAssert/eaassert.h>
 #include <EAStdC/EASprintf.h>
 #include <cwchar>
+#endif
+
 
 namespace EA {
 namespace StdC {
 
+#if defined(JN516x) || defined(CPU_JN518X)
+EASTDC_API int Vsnprintf(char8_t* EA_RESTRICT pDestination, size_t n, const char8_t* EA_RESTRICT pFormat, va_list arguments)
+	{ return vsnprintf_(pDestination, n, pFormat, arguments); }
+// char16_t, char32_t is not defined so far.
+#else
 EASTDC_API int Vsnprintf(char8_t* EA_RESTRICT pDestination, size_t n, const char8_t* EA_RESTRICT pFormat, va_list arguments)
 	{ return vsnprintf(pDestination, n, pFormat, arguments); }
 
@@ -25,5 +38,6 @@ EASTDC_API int Vsnprintf(char32_t* EA_RESTRICT pDestination, size_t n, const cha
 EASTDC_API int Vsnprintf(wchar_t* EA_RESTRICT pDestination, size_t n, const wchar_t* EA_RESTRICT pFormat, va_list arguments)
 	{ return vswprintf(pDestination, n, pFormat, arguments); }
 #endif
+#endif // JN516x || CPU_JN518X
 
 }}
